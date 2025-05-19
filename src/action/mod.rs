a388ff1146f92a8625abbfe654c1c15788e88446
Pierre Fouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-05-19 21:00:53 +0800

    feat(action): position diminishes to desperate

--- src/action/mod.rs
+++ src/action/mod.rs
@@ -13,6 +13,14 @@ impl Position {
             Position::Controlled => Position::Controlled,
         }
     }
+
+    pub fn diminish(&self) -> Self {
+        match self {
+            Position::Controlled => Position::Risky,
+            Position::Risky => Position::Desperate,
+            Position::Desperate => Position::Desperate,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -27,5 +35,13 @@ mod tests {
     #[case::from_controlled_to_controlled(Position::Controlled, Position::Controlled)]
     fn test_position_improves(#[case] from: Position, #[case] to: Position) {
         assert_eq!(from.improve(), to);
+    }
+
+    #[rstest]
+    #[case::from_controlled_to_risky(Position::Controlled, Position::Risky)]
+    #[case::from_risky_to_desperate(Position::Risky, Position::Desperate)]
+    #[case::from_desperate_to_desperate(Position::Desperate, Position::Desperate)]
+    fn test_position_diminishes(#[case] from: Position, #[case] to: Position) {
+        assert_eq!(from.diminish(), to)
     }
 }
