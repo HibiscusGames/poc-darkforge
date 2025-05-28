31c898c6e6e9288652d5fc7d054dee927016e215
Pierre Fouilloux <7494211+pfouilloux@users.noreply.github.com>
2025-05-28 22:39:11 +0800

    refactor(stress): move stress & trauma trackers from charcter into stress module

--- src/character.rs
+++ src/character.rs
@@ -15,10 +15,12 @@ use std::{
 use enum_map::{Enum, EnumMap};
 use thiserror::Error;
 
-use crate::data::{ArrayTracker, Error as DataError, Tracker, UnsignedInteger, Value};
+use crate::{
+    data::{ArrayTracker, Error as DataError, Tracker, UnsignedInteger},
+    stress::{Stress, Traumas},
+};
 
 const ACTION_MAX: usize = 4;
-const STRESS_MAX: usize = 10;
 
 #[derive(Debug, Error, PartialEq)]
 pub enum Error {
@@ -67,29 +69,6 @@ pub enum Action {
     Consort,
     /// When you `Sway`, you influence with guile, charm or argument.
     Sway,
-}
-
-/// A trauma is a persistent emotional or psychological condition that affects a character's behaviour and outlook.
-/// It is gained as a consequence of maxing out the stress meter during a heist.
-/// Traumas can be an effective way to gain experience if they are allowed to affect the heist. But this can have consequences.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum Trauma {
-    /// You’re not moved by emotional appeals or social bonds.
-    Cold,
-    /// You’re often lost in reverie, reliving past horrors, seeing things.
-    Haunted,
-    /// You’re enthralled by one thing: an activity, a person, an ideology.
-    Obsessed,
-    /// You imagine danger everywhere; you can’t trust others.
-    Paranoid,
-    /// You have little regard for your own safety or best interests.
-    Reckless,
-    /// You lose your edge; you become sentimental, passive, gentle.
-    Soft,
-    /// Your emotional state is volatile. You can instantly rage, or fall into despair, act impulsively, or freeze up.
-    Unstable,
-    /// You seek out opportunities to hurt people, even for no good reason.
-    Vicious,
 }
 
 /// Represents physical injuries a character can sustain during play.
@@ -268,9 +247,6 @@ pub struct Character {
 #[derive(Debug, Default, PartialEq)]
 struct Actions(EnumMap<Action, ActionValue>);
 type ActionValue = UnsignedInteger<u8, 0, ACTION_MAX>;
-type Stress = UnsignedInteger<u8, 0, STRESS_MAX>;
-type Traumas = ArrayTracker<Trauma, 4>;
-
 /// A specific instance of harm, including the level and type.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Harm(HarmLevel, HarmType);
@@ -416,11 +392,6 @@ impl Character {
     fn harm_mut(&mut self) -> &mut HarmTracker {
         &mut self.harm
     }
-
-    /// Returns true if the character has pending trauma (stress level at maximum)
-    pub fn has_pending_trauma(&self) -> bool {
-        self.stress.get() >= STRESS_MAX as u8
-    }
 }
 
 impl Deref for Actions {
@@ -458,7 +429,7 @@ mod tests {
     use rstest::rstest;
 
     use super::*;
-    use crate::data::{Tracker, value::Error as ValueError};
+    use crate::data::{Tracker, Value, value::Error as ValueError};
 
     const LEVELS: &[HarmLevel] = &[HarmLevel::Lesser, HarmLevel::Moderate, HarmLevel::Severe];
 
@@ -522,47 +493,6 @@ mod tests {
             }
 
             assert_eq!(4, character.action(action).get(), "Action rating should clamp precisely to MAX (4)");
-        }
-
-        #[test]
-        fn test_set_and_get_stress_level_between_0_and_10(
-            stress in 0u8..=10u8
-        ) {
-            let mut character = Character::new("Test Character");
-
-            character.stress_mut().set(stress).expect("should have set stress level");
-
-            assert_eq!(stress, character.stress().get());
-        }
-
-        #[test]
-        fn test_setting_stress_levels_above_max_clamp_to_max(
-            stress in 11u8..=u8::MAX
-        ) {
-            let mut character = Character::new("Test Character");
-
-            match character.stress_mut().set(stress).expect_err("should have clamped") {
-                DataError::Value(ValueError::ClampedMax) => assert!(stress > 10, "Stress level clamped when it was lower than max"),
-                e => panic!("unexpected error: {e:?}")
-            }
-
-            assert_eq!(STRESS_MAX as u8, character.stress().get(), "Stress level should clamp precisely to MAX ({STRESS_MAX})");
-        }
-
-        #[test]
-        fn test_incrementing_stress_levels_above_max_clamp_to_max(
-            stress in 0u8..=10u8
-        ) {
-            let mut character = Character::new("Test Character");
-            let increment = (STRESS_MAX + 1) as u8 - stress;
-
-            character.stress_mut().set(stress).expect("should have set stress level");
-            match character.stress_mut().increment(increment).expect_err("should have clamped") {
-                DataError::Value(ValueError::ClampedMax) => assert!(stress + increment > STRESS_MAX as u8, "Stress level clamped when it was lower than max ({stress} + {increment} < {STRESS_MAX})"),
-                e => panic!("unexpected error: {e:?}")
-            }
-
-            assert_eq!(STRESS_MAX as u8, character.stress().get(), "Stress level should clamp precisely to MAX ({STRESS_MAX})");
         }
 
         #[test]
@@ -677,35 +607,5 @@ mod tests {
             Error::HarmTrackerError(got) => assert_eq!(got, expect),
             e => panic!("unexpected error: {e:?}"),
         }
-    }
-
-    #[test]
-    fn test_returns_false_when_checking_for_pending_trauma_given_a_character_with_a_stress_level_below_10() {
-        let mut character = Character::new("Test Character");
-        character.stress_mut().set(9).expect("should have set stress level");
-
-        assert!(!character.has_pending_trauma());
-    }
-
-    #[test]
-    fn test_returns_true_when_checking_for_pending_trauma_given_a_character_with_a_stress_level_of_10() {
-        let mut character = Character::new("Test Character");
-        character.stress_mut().set(10).expect("should have set stress level");
-
-        assert!(character.has_pending_trauma());
-    }
-
-    #[test]
-    fn test_new_character_has_empty_trauma_list() {
-        let character = Character::new("Test Character");
-
-        assert!(character.traumas().is_empty());
-    }
-
-    #[test]
-    fn test_new_character_has_empty_harm_tracker() {
-        let character = Character::new("Test Character");
-
-        assert!(character.harm().is_empty());
     }
 }
